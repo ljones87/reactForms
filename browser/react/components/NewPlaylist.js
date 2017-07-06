b4508355fb532720848960f7c95875a602709188
Min Hwangbo <min.hwangbo@gmail.com>
2017-07-06 18:09:11 -0400

    3 - New Playlist State

---
+++ browser/react/components/NewPlaylist.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react'
+
+export default class NewPlaylist extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  handleSubmit() {
+    console.log(this.state.input);
+  }
+
+  render() {
+    return (
+      <div className="well">
+        <form className="form-horizontal" onSubmit={this.handleSubmit}>
+          <fieldset>
+            <legend>New Playlist</legend>
+            <div className="form-group">
+              <label className="col-xs-2 control-label">Name</label>
+              <div className="col-xs-10">
+                <input value={this.state.input} className="form-control" type="text" onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-xs-10 col-xs-offset-2">
+                <button type="submit" className="btn btn-success">Create Playlist</button>
+              </div>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    )
+  }
+
+}
